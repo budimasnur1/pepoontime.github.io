f3d66d1e118e2173d8cde9f7f65146feca0360fe
budimasnur1 <117183537+budimasnur1@users.noreply.github.com>
2022-11-13 22:25:07 +0700

    Create 320x50.js

---
+++ 320x50.js
@@ -0,0 +1,10 @@
+<script type="text/javascript">
+	atOptions = {
+		'key' : 'c80e8cd7e7c6f58a14a8d729f8cdad80',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://hungryrise.com/c80e8cd7e7c6f58a14a8d729f8cdad80/invoke.js"></scr' + 'ipt>');
+</script>
